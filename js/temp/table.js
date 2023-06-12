79cb3c384240ab5a0573c7f6845e49e601e83bf4
Febriand1 <fdirga63@gmail.com>
2023-06-12 21:54:50 +0700

    gg

--- js/temp/table.js
+++ js/temp/table.js
@@ -3,9 +3,6 @@ export let isiTabel = `
     <th class="whitespace-nowrap px-4 bg-white text-left">
         <div class="flex items-center -m-2">
             <div class="w-auto p-2">
-                <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
-            </div>
-            <div class="w-auto p-2">
                 <p class="text-xs font-semibold text-coolGray-800">#NPM#</p>
                 <p class="text-xs font-medium text-coolGray-500">#NAMA#</p>
                 <p class="text-xs font-medium text-coolGray-500">#NOHP#</p>
