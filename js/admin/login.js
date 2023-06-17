dab3051fa52c041a45474ca0d5700af20cc70fc2
Febriand1 <fdirga63@gmail.com>
2023-06-17 13:10:25 +0700

    hh

--- js/admin/login.js
+++ js/admin/login.js
@@ -1,3 +1,33 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  var data = {
+    username: username,
+    password: password,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        window.location.href = "index.html"; // Redirect ke halaman setelah login berhasil
+      } else {
+        document.getElementById("loginErrorMessage").style.display = "block";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
 // document.getElementById("loginForm").addEventListener("submit", function (e) {
 //   e.preventDefault();
 //   var username = document.getElementById("username").value;
@@ -13,36 +43,36 @@
 //   }
 // });
 
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  var target_url = "https://ws-nilai.herokuapp.com/admin";
+// document.getElementById("loginForm").addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+//   var target_url = "https://ws-nilai.herokuapp.com/admin";
 
-  var data = {
-    username: username,
-    password: password,
-  };
+//   var data = {
+//     username: username,
+//     password: password,
+//   };
 
-  var requestOptions = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ data }),
-  };
+//   var requestOptions = {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ data }),
+//   };
 
-  fetch(target_url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        // Redirect ke halaman setelah login berhasil
-        window.location.href = "index.html";
-      } else {
-        // Tampilkan pesan kesalahan
-        console.log(data.message);
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
+//   fetch(target_url, requestOptions)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.success) {
+//         // Redirect ke halaman setelah login berhasil
+//         window.location.href = "index.html";
+//       } else {
+//         // Tampilkan pesan kesalahan
+//         console.log(data.message);
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// });
