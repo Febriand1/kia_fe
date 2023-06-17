34c1403cdafe740444dd071e71efe27680b2e3b2
Febriand1 <fdirga63@gmail.com>
2023-06-17 13:06:24 +0700

    ehe

--- js/admin/login.js
+++ js/admin/login.js
@@ -19,11 +19,16 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   var password = document.getElementById("password").value;
   var target_url = "https://ws-nilai.herokuapp.com/admin";
 
+  var data = {
+    username: username,
+    password: password,
+  };
+
   var requestOptions = {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ data }),
   };
 
   fetch(target_url, requestOptions)
---
+++ js/admin/post_login.js
@@ -0,0 +1,46 @@
+import { postData } from "https://bukulapak.github.io/api/process.js";
+import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
+import { urlPOST, AmbilResponse } from "../config/url_post.js";
+
+function pushData() {
+  let data = {
+    alltugas: {
+      tugas1: parseInt(getValue("tugas1")),
+      tugas2: parseInt(getValue("tugas2")),
+      tugas3: parseInt(getValue("tugas3")),
+      tugas4: parseInt(getValue("tugas4")),
+      tugas5: parseInt(getValue("tugas5")),
+    },
+    uts: parseInt(getValue("uts")),
+    uas: parseInt(getValue("uas")),
+    grade: {
+      namagrade: getValue("namagrade"),
+      skala: getValue("skala"),
+    },
+    kategori: {
+      nama_mk: getValue("nama_mk"),
+      sks: parseInt(getValue("sks")),
+      jadwal: {
+        jammasuk: getValue("jammasuk"),
+        jamkeluar: getValue("jamkeluar"),
+        hari: getValue("hari").split(","),
+      },
+      pengampu: {
+        namadosen: getValue("namadosen"),
+        nik: getValue("nik"),
+        phonemumberd: getValue("phonenumberd"),
+      },
+    },
+    absensi: {
+      jumlahkehadiran: parseInt(getValue("jumlahkehadiran")),
+      biodata: {
+        nama: getValue("nama"),
+        npm: parseInt(getValue("npm")),
+        phonenumber: getValue("phonenumber"),
+      },
+    },
+  };
+  postData(urlPOST, data, AmbilResponse);
+}
+
+onClick("button", pushData);
