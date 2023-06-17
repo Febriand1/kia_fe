5bb7889bee72fd71b0ad584e3d8a6275b6eaa278
Febriand1 <fdirga63@gmail.com>
2023-06-17 13:49:55 +0700

    aa

--- js/admin/login.js
+++ js/admin/login.js
@@ -1,35 +1,26 @@
-// import { postData } from "https://bukulapak.github.io/api/process.js";
-// import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-// import { urlPOST, AmbilResponse } from "url_login.js";
-
 document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  var target_url = "https://ws-nilai.herokuapp.com/insadmin";
 
   var data = {
     username: username,
     password: password,
   };
 
-  var requestOptions = {
+  fetch("/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ data }),
-  };
-
-  fetch(target_url, requestOptions)
+    body: JSON.stringify(data),
+  })
     .then((response) => response.json())
     .then((data) => {
       if (data.success) {
-        // Redirect ke halaman setelah login berhasil
-        window.location.href = "../../template/index.html";
+        window.location.href = "index.html"; // Redirect ke halaman setelah login berhasil
       } else {
-        // Tampilkan pesan kesalahan
-        console.log(data.message);
+        document.getElementById("loginErrorMessage").style.display = "block";
       }
     })
     .catch((error) => console.error("Error:", error));
