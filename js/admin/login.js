f05ac1c0ae998dc3bb2311ce696b0fe283e4d595
Febriand1 <fdirga63@gmail.com>
2023-06-17 18:03:13 +0700

    ege

--- js/admin/login.js
+++ js/admin/login.js
@@ -19,26 +19,33 @@ function loginAdmin(event) {
     },
     body: JSON.stringify(data),
   })
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Login failed");
-      }
+    .then((response) => response.json())
+    .then((result) => {
+      alert(result.message);
+      window.location.href = "../../template/index.html";
+      // location.reload();
     })
-    .then(function (data) {
-      if (data.success) {
-        // Login berhasil
-        document.getElementById("message").innerHTML = "Login berhasil!";
-        // Redirect ke halaman admin
-        window.location.href = "../../template/index.html";
-      } else {
-        // Login gagal
-        document.getElementById("message").innerHTML = "Username atau password salah";
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-      document.getElementById("message").innerHTML = "Terjadi kesalahan saat login";
-    });
+    .catch((error) => console.log("Error:", error));
+  // .then(function (response) {
+  //   if (response.ok) {
+  //     return response.json();
+  //   } else {
+  //     throw new Error("Login failed");
+  //   }
+  // })
+  // .then(function (data) {
+  //   if (data.success) {
+  //     // Login berhasil
+  //     document.getElementById("message").innerHTML = "Login berhasil!";
+  //     // Redirect ke halaman admin
+  //     window.location.href = "../../template/index.html";
+  //   } else {
+  //     // Login gagal
+  //     document.getElementById("message").innerHTML = "Username atau password salah";
+  //   }
+  // })
+  // .catch(function (error) {
+  //   console.error(error);
+  //   document.getElementById("message").innerHTML = "Terjadi kesalahan saat login";
+  // });
 }
