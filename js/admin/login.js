8ebfee31476056fb3f765bef231cd7c4b2aafa5e
Febriand1 <fdirga63@gmail.com>
2023-06-17 17:24:21 +0700

    gg

--- js/admin/login.js
+++ js/admin/login.js
@@ -5,7 +5,7 @@ function loginAdmin(event) {
 
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  var target_url = "/https://ws-nilai.herokuapp.com/loginadmin";
+  var target_url = "https://ws-nilai.herokuapp.com/loginadmin";
 
   var data = {
     username: username,
