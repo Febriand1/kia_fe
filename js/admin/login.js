637905411f8b786ae1ecff7ef0f5a327a4e904de
Febriand1 <fdirga63@gmail.com>
2023-06-18 00:23:21 +0700

    hh

--- js/admin/login.js
+++ js/admin/login.js
@@ -1,46 +1,36 @@
-import { postData } from "https://bukulapak.github.io/api/process.js";
-import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPOST, AmbilResponse } from "url_admin.js";
+document.getElementById("loginForm").addEventListener("submit", loginAdmin);
 
 function loginAdmin(event) {
   event.preventDefault();
-  let datajson = {
-    username: getValue("username"),
-    password: getValue("password"),
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var target_url = "https://ws-nilai.herokuapp.com/loginadmin";
+
+  var data = {
+    username: username,
+    password: password,
   };
-  postData(urlPOST, datajson, AmbilResponse);
+
+  var raw = JSON.stringify(data);
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      alert(result.message);
+      if (result.status) {
+        window.location.href = "index.html";
+      }
+    })
+    .catch((error) => console.log("Error:", error));
 }
-onClick("submit", loginAdmin);
-
-// document.getElementById("loginForm").addEventListener("submit", loginAdmin);
-
-// function loginAdmin(event) {
-//   event.preventDefault();
-
-//   var username = document.getElementById("username").value;
-//   var password = document.getElementById("password").value;
-//   var target_url = "https://ws-nilai.herokuapp.com/loginadmin";
-
-//   var data = {
-//     username: username,
-//     password: password,
-//   };
-
-//   fetch(target_url, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   })
-//     .then((response) => response.json())
-//     .then((result) => {
-//       alert(result.message);
-//       if (result.status === true) {
-//         window.location.href = "index.html";
-//       } else {
-//         window.location.href = "p.html";
-//       }
-//     })
-//     .catch((error) => console.log("Error:", error));
-// }
