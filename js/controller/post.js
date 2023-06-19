307a76cf8cd3de1d435c0431bc110b1eb83904f3
Febriand1 <fdirga63@gmail.com>
2023-06-19 15:17:11 +0700

    ehe

--- js/controller/post.js
+++ js/controller/post.js
@@ -1,11 +1,10 @@
-import { postData } from "https://bukulapak.github.io/api/process.js";
-import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPOST, AmbilResponse } from "../config/url_post.js";
+import { getValue, setValue } from "https://bukulapak.github.io/element/process.js";
 import { hitungRataRata } from "./post_avg.js";
+import { postData } from "https://bukulapak.github.io/api/process.js";
+import { onClick } from "https://bukulapak.github.io/element/process.js";
+import { urlPOST, AmbilResponse } from "../config/url_post.js";
 
 function pushData() {
-  let hasilPerhitungan = hitungRataRata(alltugas, uts, uas);
-
   let data = {
     alltugas: {
       tugas1: parseInt(getValue("tugas1")),
@@ -17,8 +16,8 @@ function pushData() {
     uts: parseInt(getValue("uts")),
     uas: parseInt(getValue("uas")),
     grade: {
-      namagrade: hasilPerhitungan.rataRata.toFixed(2),
-      skala: hasilPerhitungan.skala,
+      namagrade: getValue("namagrade"),
+      skala: getValue("skala"),
     },
     kategori: {
       nama_mk: getValue("nama_mk"),
@@ -47,3 +46,85 @@ function pushData() {
 }
 
 onClick("button", pushData);
+
+// Mendapatkan referensi ke elemen form
+const tugas1Input = document.getElementById("tugas1");
+const tugas2Input = document.getElementById("tugas2");
+const tugas3Input = document.getElementById("tugas3");
+const tugas4Input = document.getElementById("tugas4");
+const tugas5Input = document.getElementById("tugas5");
+const utsInput = document.getElementById("uts");
+const uasInput = document.getElementById("uas");
+const namagradeInput = document.getElementById("namagrade");
+const skalaInput = document.getElementById("skala");
+
+// Menambahkan event listener ke form tugas1 sampai tugas5, uts, dan uas
+[tugas1Input, tugas2Input, tugas3Input, tugas4Input, tugas5Input, utsInput, uasInput].forEach((input) => {
+  input.addEventListener("input", updateGradeAndSkala);
+});
+
+// Fungsi untuk mengupdate nilai pada form namagrade dan skala
+function updateGradeAndSkala() {
+  // Mendapatkan nilai dari form tugas1 sampai tugas5, uts, dan uas
+  const tugas1 = parseInt(getValue("tugas1"));
+  const tugas2 = parseInt(getValue("tugas2"));
+  const tugas3 = parseInt(getValue("tugas3"));
+  const tugas4 = parseInt(getValue("tugas4"));
+  const tugas5 = parseInt(getValue("tugas5"));
+  const uts = parseInt(getValue("uts"));
+  const uas = parseInt(getValue("uas"));
+
+  // Menghitung rata-rata
+  const hasilPerhitungan = hitungRataRata({ tugas1, tugas2, tugas3, tugas4, tugas5 }, uts, uas);
+
+  // Mengisi nilai pada form namagrade dan skala
+  namagradeInput.value = hasilPerhitungan.rataRata.toFixed(2);
+  skalaInput.value = hasilPerhitungan.skala;
+}
+
+// import { postData } from "https://bukulapak.github.io/api/process.js";
+// import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
+// import { urlPOST, AmbilResponse } from "../config/url_post.js";
+
+// function pushData() {
+//   let data = {
+//     alltugas: {
+//       tugas1: parseInt(getValue("tugas1")),
+//       tugas2: parseInt(getValue("tugas2")),
+//       tugas3: parseInt(getValue("tugas3")),
+//       tugas4: parseInt(getValue("tugas4")),
+//       tugas5: parseInt(getValue("tugas5")),
+//     },
+//     uts: parseInt(getValue("uts")),
+//     uas: parseInt(getValue("uas")),
+//     grade: {
+//       namagrade: getValue("namagrade"),
+//       skala: getValue("skala"),
+//     },
+//     kategori: {
+//       nama_mk: getValue("nama_mk"),
+//       sks: parseInt(getValue("sks")),
+//       jadwal: {
+//         jammasuk: getValue("jammasuk"),
+//         jamkeluar: getValue("jamkeluar"),
+//         hari: getValue("hari").split(","),
+//       },
+//       pengampu: {
+//         namadosen: getValue("namadosen"),
+//         nik: getValue("nik"),
+//         phonenumberd: getValue("phonenumberd"),
+//       },
+//     },
+//     absensi: {
+//       jumlahkehadiran: parseInt(getValue("jumlahkehadiran")),
+//       biodata: {
+//         nama: getValue("nama"),
+//         npm: parseInt(getValue("npm")),
+//         phonenumber: getValue("phonenumber"),
+//       },
+//     },
+//   };
+//   postData(urlPOST, data, AmbilResponse);
+// }
+
+// onClick("button", pushData);
--- js/controller/post_avg.js
+++ js/controller/post_avg.js
@@ -4,26 +4,28 @@ export function hitungRataRata(alltugas, uts, uas) {
   let tugas3 = parseInt(alltugas.tugas3);
   let tugas4 = parseInt(alltugas.tugas4);
   let tugas5 = parseInt(alltugas.tugas5);
+  let uts = parseInt(uts);
+  let uas = parseInt(uas);
 
   let jumlahTugas = tugas1 + tugas2 + tugas3 + tugas4 + tugas5;
-  let totalNilai = jumlahTugas + parseInt(uts) + parseInt(uas);
+  let totalNilai = jumlahTugas + uts + uas;
   let rataRata = totalNilai / 7;
 
-  let skala = "";
+  let namagrade = "";
   if (rataRata >= 80) {
-    skala = "A";
+    namagrade = "A";
   } else if (rataRata >= 70) {
-    skala = "B";
+    namagrade = "B";
   } else if (rataRata >= 60) {
-    skala = "C";
+    namagrade = "C";
   } else if (rataRata >= 50) {
-    skala = "D";
+    namagrade = "D";
   } else {
-    skala = "E";
+    namagrade = "E";
   }
 
   return {
     rataRata: rataRata,
-    skala: skala,
+    namagrade: namagrade,
   };
 }
