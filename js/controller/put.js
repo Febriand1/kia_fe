db1695cf79080e0e22475e11878288175d4898a1
Febriand1 <fdirga63@gmail.com>
2023-06-19 16:16:09 +0700

    ehe

--- js/controller/put.js
+++ js/controller/put.js
@@ -2,6 +2,49 @@ import { putData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPUT, AmbilResponse } from "../config/url_put.js";
 
+const namagradeInput = getValue("namagrade");
+const skalaInput = getValue("skala");
+
+function hitungRataRata(tugas, uts, uas) {
+  const jumlahTugas = tugas.tugas1 + tugas.tugas2 + tugas.tugas3 + tugas.tugas4 + tugas.tugas5;
+  const totalNilai = jumlahTugas + uts + uas;
+  const rataRata = totalNilai / 7;
+
+  let skala;
+  if (rataRata >= 80) {
+    skala = "A";
+  } else if (rataRata >= 70) {
+    skala = "B";
+  } else if (rataRata >= 60) {
+    skala = "C";
+  } else if (rataRata >= 50) {
+    skala = "D";
+  } else {
+    skala = "E";
+  }
+
+  return { rataRata, skala };
+}
+
+function updateGradeAndSkala() {
+  const tugas1 = parseInt(getValue("tugas1"));
+  const tugas2 = parseInt(getValue("tugas2"));
+  const tugas3 = parseInt(getValue("tugas3"));
+  const tugas4 = parseInt(getValue("tugas4"));
+  const tugas5 = parseInt(getValue("tugas5"));
+  const uts = parseInt(getValue("uts"));
+  const uas = parseInt(getValue("uas"));
+
+  // Menghitung rata-rata
+  const hasilPerhitungan = hitungRataRata({ tugas1, tugas2, tugas3, tugas4, tugas5 }, uts, uas);
+
+  // Mengisi nilai pada form namagrade dan skala
+  setValue("namagrade", hasilPerhitungan.skala);
+  setValue("skala", hasilPerhitungan.rataRata.toFixed(2));
+  // namagradeInput.value = hasilPerhitungan.rataRata.toFixed(2);
+  // skalaInput.value = hasilPerhitungan.skala;
+}
+
 function pushData() {
   let data = {
     alltugas: {
@@ -44,3 +87,8 @@ function pushData() {
 }
 
 onClick("button1", pushData);
+
+const inputElements = document.querySelectorAll('input[type="text"], input[type="number"]');
+inputElements.forEach((input) => {
+  input.addEventListener("input", updateGradeAndSkala);
+});
