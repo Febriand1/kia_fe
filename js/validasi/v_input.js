8562929e346af4871c121a87da1bd2e8c3df5051
Febriand1 <fdirga63@gmail.com>
2023-06-23 14:08:48 +0700

    f

---
+++ js/validasi/v_input.js
@@ -0,0 +1,21 @@
+function validateForm() {
+  // Mendapatkan nilai input dari elemen
+  var nama = document.getElementById("nama").value;
+
+  // Mendapatkan elemen untuk menampilkan pesan kesalahan
+  var errorContainer = document.getElementById("error-container");
+  var errorMessage = document.getElementById("error-message");
+
+  // Menghapus pesan kesalahan sebelumnya (jika ada)
+  errorContainer.style.display = "none";
+
+  // Validasi nama
+  if (nama == "") {
+    errorMessage.innerHTML = "Nama harus diisi";
+    errorContainer.style.display = "block";
+    return false;
+  }
+
+  // Jika semua validasi berhasil
+  return true;
+}
