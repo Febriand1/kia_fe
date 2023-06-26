e0616f3a2251a5b3b2309dfdd8749dece363aa80
Febriand1 <fdirga63@gmail.com>
2023-06-26 16:33:48 +0700

    gg

--- js/validasi/validasi.js
+++ js/validasi/validasi.js
@@ -1,21 +1,44 @@
-export function validasiForm() {
-  // Mendapatkan nilai input dari elemen
-  var nama = document.getElementById("nama").value;
+export function validasiForm(data) {
+  // Validasi tugas
+  for (let tugas in data.alltugas) {
+    if (isNaN(data.alltugas[tugas])) {
+      alert("Nilai tugas harus berupa angka");
+      return false;
+    }
+  }
 
-  // Mendapatkan elemen untuk menampilkan pesan kesalahan
-  var errorContainer = document.getElementById("error-container");
-  var errorMessage = document.getElementById("error-message");
-
-  // Menghapus pesan kesalahan sebelumnya (jika ada)
-  errorContainer.style.display = "none";
-
-  // Validasi nama
-  if (nama == "") {
-    errorMessage.innerHTML = "Nama harus diisi";
-    errorContainer.style.display = "block";
+  // Validasi nilai UTS dan UAS
+  if (isNaN(data.uts) || isNaN(data.uas)) {
+    alert("Nilai UTS dan UAS harus berupa angka");
     return false;
   }
 
-  // Jika semua validasi berhasil
+  // Validasi nama grade dan skala
+  if (data.grade.namagrade.trim() === "" || data.grade.skala.trim() === "") {
+    alert("Nama grade dan skala harus diisi");
+    return false;
+  }
+
+  // Validasi kategori
+  if (
+    data.kategori.nama_mk.trim() === "" ||
+    isNaN(data.kategori.sks) ||
+    data.kategori.jadwal.jammasuk.trim() === "" ||
+    data.kategori.jadwal.jamkeluar.trim() === "" ||
+    data.kategori.jadwal.hari.length === 0 ||
+    data.kategori.pengampu.namadosen.trim() === "" ||
+    isNaN(data.kategori.pengampu.nik) ||
+    data.kategori.pengampu.phonenumberd.trim() === ""
+  ) {
+    alert("Data kategori harus diisi dengan benar");
+    return false;
+  }
+
+  // Validasi absensi
+  if (isNaN(data.absensi.jumlahkehadiran) || data.absensi.biodata.nama.trim() === "" || isNaN(data.absensi.biodata.npm) || data.absensi.biodata.phonenumber.trim() === "") {
+    alert("Data absensi harus diisi dengan benar");
+    return false;
+  }
+
   return true;
 }
