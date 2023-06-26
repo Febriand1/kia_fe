0697d8a69a11b1a0a9242c81c98e4f2112881618
Febriand1 <fdirga63@gmail.com>
2023-06-26 17:51:52 +0700

    ee

--- js/validasi/validasi.js
+++ js/validasi/validasi.js
@@ -1,44 +1,16 @@
-export function validasiForm(data) {
-  // Validasi tugas
-  for (let tugas in data.alltugas) {
-    if (isNaN(data.alltugas[tugas])) {
-      alert("Nilai tugas harus berupa angka");
-      return false;
-    }
-  }
+function validateForm(event) {
 
-  // Validasi nilai UTS dan UAS
-  if (isNaN(data.uts) || isNaN(data.uas)) {
-    alert("Nilai UTS dan UAS harus berupa angka");
-    return false;
-  }
+  var nameInput = document.getElementById("name");
+  var nameError = document.getElementById("nameError");
 
-  // Validasi nama grade dan skala
-  if (data.grade.namagrade.trim() === "" || data.grade.skala.trim() === "") {
-    alert("Nama grade dan skala harus diisi");
-    return false;
-  }
+  // Menghapus pesan error sebelumnya
+  nameError.innerHTML = "";
 
-  // Validasi kategori
-  if (
-    data.kategori.nama_mk.trim() === "" ||
-    isNaN(data.kategori.sks) ||
-    data.kategori.jadwal.jammasuk.trim() === "" ||
-    data.kategori.jadwal.jamkeluar.trim() === "" ||
-    data.kategori.jadwal.hari.length === 0 ||
-    data.kategori.pengampu.namadosen.trim() === "" ||
-    isNaN(data.kategori.pengampu.nik) ||
-    data.kategori.pengampu.phonenumberd.trim() === ""
-  ) {
-    alert("Data kategori harus diisi dengan benar");
-    return false;
+  // Validasi form
+  if (nameInput.value === "") {
+    nameInput.classList.add("error");
+    nameError.innerHTML = "Nama harus diisi";
+  } else {
+    nameInput.classList.remove("error");
   }
-
-  // Validasi absensi
-  if (isNaN(data.absensi.jumlahkehadiran) || data.absensi.biodata.nama.trim() === "" || isNaN(data.absensi.biodata.npm) || data.absensi.biodata.phonenumber.trim() === "") {
-    alert("Data absensi harus diisi dengan benar");
-    return false;
-  }
-
-  return true;
 }
