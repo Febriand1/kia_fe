9583002eb86dac5324c60cd4c47f2e0c4a018289
Febriand1 <fdirga63@gmail.com>
2023-07-01 13:21:19 +0700

    ee

--- js/tambahan/validasi.js
+++ js/tambahan/validasi.js
@@ -1,10 +1,10 @@
 export function validasiForm(event) {
+  event.preventDefault();
   var nama = document.getElementById("nama").value;
   var errorText = document.getElementById("errorText");
 
   if (nama === "") {
     errorText.textContent = "nama tidak boleh kosong!";
-    event.preventDefault();
   } else {
     errorText.textContent = "";
   }
