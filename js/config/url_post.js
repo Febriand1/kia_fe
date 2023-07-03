0a31a26d444f3a3f1272b4f9874668cd41b92609
Febriand1 <fdirga63@gmail.com>
2023-07-03 16:53:13 +0700

    ehe

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -3,5 +3,5 @@ export let urlPOST = "https://ws-nilai.herokuapp.com/insnilai";
 export function AmbilResponse(result) {
   console.log(result); //menampilkan response API pada console
   alert(result.message); //menampilkan response API pada alert
-  window.location.reload(); //reload halaman setelah klik ok pada alert
+  window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
 }
