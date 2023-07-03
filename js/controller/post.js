4bb1f17b9b7c23b59ae3deb8bd9cc8df67c51562
Febriand1 <fdirga63@gmail.com>
2023-07-03 14:47:38 +0700

    ee

--- js/controller/post.js
+++ js/controller/post.js
@@ -2,12 +2,8 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url_post.js";
 import { updateGradeAndSkala } from "../tambahan/rata-rata.js";
-// import { validasiForm } from "../tambahan/validasi.js";
 
 function pushData() {
-  // if (!validasiForm()) {
-  //   return;
-  // }
   let data = {
     alltugas: {
       tugas1: parseInt(getValue("tugas1")),
