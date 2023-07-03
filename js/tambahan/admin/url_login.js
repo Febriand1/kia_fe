2f5711b0fea0601ad11763ff0f2fa02854f1fd9f
Febriand1 <fdirga63@gmail.com>
2023-07-03 18:07:03 +0700

    ehe

--- js/tambahan/admin/url_login.js
+++ js/tambahan/admin/url_login.js
@@ -1,8 +1,8 @@
 export let urlLOGIN = "https://ws-nilai.herokuapp.com/loginadmin";
 
 export function AmbilResponse(result) {
-  alert(result.message);
   console.log(result.status);
+  alert(result.message);
   if (result.status === 200) {
     window.location.href = "index.html";
   }
