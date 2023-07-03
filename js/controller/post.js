dd29925ca8249f8ba68463748fa6acfdcf3f2c64
Febriand1 <fdirga63@gmail.com>
2023-07-03 11:11:47 +0700

    ee

--- js/controller/post.js
+++ js/controller/post.js
@@ -2,7 +2,6 @@ import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
 import { urlPOST, AmbilResponse } from "../config/url_post.js";
 import { updateGradeAndSkala } from "../tambahan/rata-rata.js";
-import { validasiForm } from "../tambahan/validasi.js";
 
 function pushData() {
   let data = {
@@ -44,9 +43,8 @@ function pushData() {
   };
   postData(urlPOST, data, AmbilResponse);
 }
-if (validasiForm) {
-  onClick("button", pushData);
-}
+
+onClick("button", pushData);
 
 const inputElements = document.querySelectorAll('input[type="text"], input[type="number"]');
 inputElements.forEach((input) => {
--- js/tambahan/validasi.js
+++ js/tambahan/validasi.js
@@ -1,15 +1,5 @@
-export function validasiForm(event) {
+function validasiForm(event) {
   event.preventDefault();
-
-  let tugas = {
-    tugas1: document.getElementById("tugas1").value,
-    tugas2: document.getElementById("tugas2").value,
-    tugas3: document.getElementById("tugas3").value,
-    tugas4: document.getElementById("tugas4").value,
-    tugas5: document.getElementById("tugas5").value,
-    uts: document.getElementById("uts").value,
-    uas: document.getElementById("uas").value,
-  };
 
   var nama = document.getElementById("nama").value;
   var npm = document.getElementById("npm").value;
@@ -23,57 +13,42 @@ export function validasiForm(event) {
   var jammasuk = document.getElementById("jammasuk").value;
   var jamkeluar = document.getElementById("jamkeluar").value;
   var hari = document.getElementById("hari").value;
+  var tugas1 = document.getElementById("tugas1").value;
+  var tugas2 = document.getElementById("tugas2").value;
+  var tugas3 = document.getElementById("tugas3").value;
+  var tugas4 = document.getElementById("tugas4").value;
+  var tugas5 = document.getElementById("tugas5").value;
+  var uts = document.getElementById("uts").value;
+  var uas = document.getElementById("uas").value;
   var namagrade = document.getElementById("namagrade").value;
   var skala = document.getElementById("skala").value;
 
-  var errorText = document.getElementById("errorText");
-
-  if (nama === "") {
-    errorText.textContent = "Nama tidak boleh kosong!";
-  }
-  if (npm === "") {
-    errorText.textContent = "NPM tidak boleh kosong!";
-  }
-  if (phonenumber === "") {
-    errorText.textContent = "No.HP Mahasiswa tidak boleh kosong!";
-  }
-  if (jumlahkehadiran === "") {
-    errorText.textContent = "Jumlah Kehadiran tidak boleh kosong!";
-  }
-  if (nama_mk === "") {
-    errorText.textContent = "Matakuliah tidak boleh kosong!";
-  }
-  if (sks === "") {
-    errorText.textContent = "SKS tidak boleh kosong!";
-  }
-  if (nik === "") {
-    errorText.textContent = "NIK tidak boleh kosong!";
-  }
-  if (namadosen === "") {
-    errorText.textContent = "Dosen tidak boleh kosong!";
-  }
-  if (phonenumberd === "") {
-    errorText.textContent = "No.HP Dosen tidak boleh kosong!";
-  }
-  if (jammasuk === "") {
-    errorText.textContent = "Jam Masuk tidak boleh kosong!";
-  }
-  if (jamkeluar === "") {
-    errorText.textContent = "Jam Keluar tidak boleh kosong!";
-  }
-  if (hari === "") {
-    errorText.textContent = "Hari tidak boleh kosong!";
-  }
-  if (tugas === "") {
-    errorText.textContent = "Tugas tidak boleh kosong!";
-  }
-  if (namagrade === "") {
-    errorText.textContent = "Grade tidak boleh kosong!";
-  }
-  if (skala === "") {
-    errorText.textContent = "Rata-Rata tidak boleh kosong!";
+  if (
+    nama != "" &&
+    npm != "" &&
+    phonenumber != "" &&
+    jumlahkehadiran != "" &&
+    nama_mk != "" &&
+    sks != "" &&
+    nik != "" &&
+    namadosen != "" &&
+    phonenumberd != "" &&
+    jammasuk != "" &&
+    jamkeluar != "" &&
+    hari != "" &&
+    tugas1 != "" &&
+    tugas2 != "" &&
+    tugas3 != "" &&
+    tugas4 != "" &&
+    tugas5 != "" &&
+    uts != "" &&
+    uas != "" &&
+    namagrade != "" &&
+    skala != ""
+  ) {
+    return true;
   } else {
-    errorText.textContent = "";
+    alert("Anda harus mengisi data dengan lengkap !");
   }
 }
 
