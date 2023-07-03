91b5bf9ff266fc150c4d42ba29d2a641e0fa65d9
Febriand1 <fdirga63@gmail.com>
2023-07-03 16:33:04 +0700

    ee

--- js/config/url_put.js
+++ js/config/url_put.js
@@ -6,6 +6,6 @@ export let urlPUT = "https://ws-nilai.herokuapp.com/updnilai/" + nilaiId;
 
 export function AmbilResponse(result) {
   console.log(result); //menampilkan response API pada console
-  confirm(result.message); //menampilkan response API pada alert
+  alert(result.message); //menampilkan response API pada alert
   window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
 }
