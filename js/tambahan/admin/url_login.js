c5a45265d656765f2acec372638d21ec1dd204f9
Febriand1 <fdirga63@gmail.com>
2023-07-06 14:37:56 +0700

    ehe

--- js/tambahan/admin/url_login.js
+++ js/tambahan/admin/url_login.js
@@ -4,8 +4,9 @@ export function AmbilResponse(result) {
   console.log(result.status);
   alert(result.message);
   if (result.status === 200) {
-    const token = result.data.token;
+    const token = result.token;
     localStorage.setItem("jwtToken", token);
+    console.log(token);
     window.location.href = "index.html";
   } else {
     return false;
