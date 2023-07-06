8412e9adc3549a518f288b702144f7ddd49d267d
Febriand1 <fdirga63@gmail.com>
2023-07-06 19:01:31 +0700

    ehee

--- js/tambahan/redirect.js
+++ js/tambahan/redirect.js
@@ -1,5 +1,11 @@
-// Check if the current page is the root page
-if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
-  // Redirect to the login admin page
-  window.location.href = "login_admin.html";
+// Memeriksa apakah pengguna telah terautentikasi
+function checkAuthentication() {
+  const token = localStorage.getItem("jwtToken");
+  if (!token) {
+    // Jika tidak ada token, arahkan pengguna ke halaman login
+    window.location.href = "login_admin.html";
+  }
 }
+
+// Panggil fungsi checkAuthentication saat halaman selesai dimuat
+window.addEventListener("DOMContentLoaded", checkAuthentication);
