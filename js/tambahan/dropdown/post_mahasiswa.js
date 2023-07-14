d06bbb6c50c3bbff96a834f0ef143cbb009d189c
Febriand1 <fdirga63@gmail.com>
2023-07-15 00:39:24 +0700

    ehe

--- js/tambahan/dropdown/post_mahasiswa.js
+++ js/tambahan/dropdown/post_mahasiswa.js
@@ -1,20 +1,36 @@
-const dropdown = document.getElementById("nama");
-const npmInput = document.getElementById("npm");
-const phoneNumberInput = document.getElementById("phonenumber");
+fetch("https://ws-nilai.herokuapp.com/mhs")
+  .then((response) => response.json())
+  .then((result) => {
+    const namaDropdown = document.getElementById("nama");
+    const npmInput = document.getElementById("npm");
+    const phoneNumberInput = document.getElementById("phonenumber");
 
-dropdown.addEventListener("change", () => {
-  const selectedOption = dropdown.options[dropdown.nama];
-  const selectedValue = selectedOption._id;
-
-  // Mengambil data mahasiswa dari API berdasarkan pilihan dropdown
-  fetch(`https://ws-nilai.herokuapp.com/mhs${selectedValue}`)
-    .then((response) => response.json())
-    .then((data) => {
-      // Mengisi nilai npm dan no.hp berdasarkan data mahasiswa yang diperoleh
-      npmInput.value = data.npm;
-      phoneNumberInput.value = data.phonenumber;
-    })
-    .catch((error) => {
-      console.error("Terjadi kesalahan:", error);
+    // Mengisi dropdown dengan opsi-opsi dari data API
+    result.forEach((item) => {
+      const option = document.createElement("option");
+      option.text = item.nama; // Teks opsi
+      option.value = item._id; // Nilai opsi
+      namaDropdown.appendChild(option);
     });
-});
+
+    // Mendengarkan event change pada dropdown
+    namaDropdown.addEventListener("change", () => {
+      const selectedOption = namaDropdown.options[namaDropdown.selectedIndex];
+      const selectedValue = selectedOption.value;
+
+      // Cari data mahasiswa yang sesuai dengan nilai yang dipilih
+      const selectedMahasiswa = result.find((item) => item._id === selectedValue);
+
+      // Mengisi nilai npm dan no.hp pada input
+      if (selectedMahasiswa) {
+        npmInput.value = selectedMahasiswa.npm;
+        phoneNumberInput.value = selectedMahasiswa.phonenumber;
+      } else {
+        npmInput.value = "";
+        phoneNumberInput.value = "";
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan:", error);
+  });
