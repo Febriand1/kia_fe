86c6ecffdaed5a4ca7464813dc471399bbe6db00
Febriand1 <fdirga63@gmail.com>
2023-07-15 15:51:05 +0700

    ehe

---
+++ js/tambahan/logout.js
@@ -0,0 +1,21 @@
+// Fungsi untuk logout
+function logout() {
+  // Menampilkan Sweet Alert konfirmasi
+
+  if (result.isConfirmed) {
+    // Menghapus token dari local storage atau cookie
+    localStorage.removeItem("jwtToken"); // Jika menggunakan local storage
+    localStorage.removeItem("previousPageURL"); // Jika menggunakan local storage
+    // document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'; // Jika menggunakan cookie
+
+    // Redirect atau lakukan tindakan lain setelah logout
+    const currentPath = window.location.pathname.split("/").pop();
+    if (currentPath === "index.html") {
+      window.location.href = "login_admin.html";
+    } else {
+      window.location.href = "login_admin.html";
+    }
+  }
+}
+
+document.getElementById("logoutButton").addEventListener("click", logout);
